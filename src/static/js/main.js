0babffae8eebf675a25cdb2fe46fda4d361b2bcc
Michael Cetrulo <mail2samus@gmail.com>
2012-10-12 19:48:45 -0200

    save model (UI)

--- src/static/js/main.js
+++ src/static/js/main.js
@@ -1,18 +1,41 @@
 $(function() {
     var WidgetDefinition = Backbone.Model.extend({
-        defaults: { "name": "new widget", "description": "this is a new widget", "dimensions": 1, "source": "{}" }
+        defaults: {
+            'name': 'new widget',
+            'description': 'this is a new widget',
+            'dimensions': 1,
+            'source': '{}'
+        },
+
+        url: 'ws/widget-definition/'
     });
 
     var WidgetDefinitionView = Backbone.View.extend({
         template: _.template($('#widget-definition-tpl').html()),
+
+        events: {
+            'click input[type="button"]': 'save'
+        },
+
         render: function() {
             this.$el.html(this.template(this.model.toJSON()));
             return this;
+        },
+
+        save: function() {
+            var data = {};
+            $('form :input', this.$el).each(function(i, e) {
+                var $e = $(e);
+                if ($e.attr('name'))
+                    data[$e.attr('name')] = $e.val();
+            });
+
+            this.model.save(data);
         }
     });
 
     var App = new WidgetDefinitionView({
-        el: $("#main"),
+        el: $('#main'),
         model: new WidgetDefinition()
     });
     App.render();
