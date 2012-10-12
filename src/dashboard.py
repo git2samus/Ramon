a11c0174a7fc30e87948cfb31d74e32cb3bf5f43
Michael Cetrulo <mail2samus@gmail.com>
2012-10-12 17:57:33 -0200

    kickstart flask/bootstrap

---
+++ src/dashboard.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+from flask import Flask, render_template
+app = Flask(__name__)
+
+@app.route('/')
+def dashboard():
+    return render_template('dashboard.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
