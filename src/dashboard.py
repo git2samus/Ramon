f383b177fbf5d5ba1402ed66b4756b644ee1bec2
Michael Cetrulo <mail2samus@gmail.com>
2012-10-12 20:17:44 -0200

    sqlite db initialization code

--- src/dashboard.py
+++ src/dashboard.py
@@ -1,10 +1,48 @@
 #!/usr/bin/env python
-from flask import Flask, render_template
+import os, sqlite3
+from contextlib import closing
+from flask import Flask, render_template, g
+
 app = Flask(__name__)
+
+
+DATABASE = 'dashboard.db'
+
+def init_db():
+    with closing(connect_db()) as db:
+        with app.open_resource('schema.sql') as f:
+            db.cursor().executescript(f.read())
+        db.commit()
+
+def connect_db():
+    return sqlite3.connect(DATABASE)
+
+def query_db(query, args=(), one=False):
+    cur = g.db.execute(query, args)
+    rv = [dict((cur.description[idx][0], value) for idx, value in enumerate(row)) for row in cur.fetchall()]
+    return (rv[0] if rv else None) if one else rv
+
+
+@app.before_request
+def before_request():
+    g.db = connect_db()
+
+@app.teardown_request
+def teardown_request(exception):
+    if hasattr(g, 'db'):
+        g.db.close()
+
+
+@app.route('/ws/widget-definition/')
+def dashboard():
+    return render_template('dashboard.html')
 
 @app.route('/')
 def dashboard():
     return render_template('dashboard.html')
 
+
 if __name__ == '__main__':
+    if not os.path.exists(DATABASE):
+        init_db()
     app.run(debug=True)
---
+++ src/schema.sql
@@ -0,0 +1,1 @@
+CREATE TABLE widget_definition (id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT NOT NULL, description TEXT NOT NULL, dimensions INTEGER NOT NULL, source TEXT NOT NULL);
