3722b8f3ff2bcb3b6a095dcf2adc8aa10871f094
Michael Cetrulo <mail2samus@gmail.com>
2012-10-14 01:03:14 -0200

    properly communicate changes on library when saving widget

--- src/static/js/main.js
+++ src/static/js/main.js
@@ -1,4 +1,6 @@
 $(function() {
+    var dispatcher = _.clone(Backbone.Events);
+
     var WidgetDefinition = Backbone.Model.extend({
         defaults: {
             'name': 'new widget',
@@ -22,10 +24,13 @@ $(function() {
         el: $('#widget-library'),
         template: _.template($('#widget-library-tpl').html()),
 
-        render: function(id) {
+        render: function() {
+            if (typeof(this.options.current_id) == 'undefined')
+                this.options.current_id = 'new';
+
             this.$el.html(this.template({
                 models: this.collection.toJSON(),
-                current_id: (typeof(id) == 'undefined')? 'new' : id,
+                current_id: this.options.current_id,
             }));
             return this;
         },
@@ -54,17 +59,30 @@ $(function() {
         },
 
         save: function() {
-            this.model.save();
+            this.model.save(null, {
+                success: function() {
+                    dispatcher.trigger('library:refresh');
+                },
+            });
         },
     });
 
 
     var widget_collection = new WidgetDefinitionList();
     var widget_library = new WidgetLibraryView({
-        collection: widget_collection
+        collection: widget_collection,
     });
 
     var widget_editor = new WidgetDefinitionView();
+
+
+    dispatcher.on('library:refresh', function() {
+        widget_collection.fetch({
+            success: function() {
+                widget_library.render();
+            },
+        });
+    });
 
 
     var Workspace = Backbone.Router.extend({
@@ -75,28 +93,28 @@ $(function() {
         },
 
         newWidget: function() {
-            widget_collection.fetch({
-                success: function() {
-                    widget_library.render('new');
+            widget_library.options.current_id = 'new';
+            widget_library.render();
 
-                    widget_editor.model = new WidgetDefinition();
-                    widget_editor.render();
-                },
-            });
+            widget_editor.model = new WidgetDefinition();
+            widget_editor.render();
         },
 
         editWidget: function(id) {
-            widget_collection.fetch({
-                success: function() {
-                    widget_library.render(id);
+            widget_library.options.current_id = id;          //TODO validation
+            widget_library.render();
 
-                    widget_editor.model = widget_collection.get(id); //TODO validation
-                    widget_editor.render();
-                },
-            });
+            widget_editor.model = widget_collection.get(id); //TODO validation
+            widget_editor.render();
         },
     });
-
     var workspace = new Workspace();
-    Backbone.history.start();
+
+
+    // load widgets - fire route afterwards (calls render)
+    widget_collection.fetch({
+        success: function() {
+            Backbone.history.start();
+        },
+    });
 });
